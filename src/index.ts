04ec5b12ea5632fec54a5b4e1d9a44abfbe118b6
Ryan Young <youngre511@gmail.com>
2025-06-26 16:11:01 -0400

    feat: add custom cyrillic romanization derived from cyrillic-to-translit-js

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export { romanizeArabic } from "./transliterators/arabic-romanization.js";
 export { romanizeJapanese } from "./transliterators/japanese-romanization.js";
 export { romanizeIndic } from "./transliterators/inidic-romanization.js";
 export { romanizeThai } from "./transliterators/thai-romanization.js";
+export { romanizeCantonese } from "./transliterators/cantonese-romanization.js";
+export { romanizeMandarin } from "./transliterators/mandarin-romanization.js";
+export { romanizeCyrillic } from "./transliterators/cyrillic-romanization.js";
 export { ConvertibleLanguage, IndicLanguage } from "./types/language-types";
--- src/transliterators/cyrillic-romanization.ts
+++ src/transliterators/cyrillic-romanization.ts
@@ -1,16 +1,373 @@
-import cyrillicToTranslit from "cyrillic-to-translit-js";
 import { CyrillicLanguage } from "../types/language-types";
 
-export const romanizeCyrillic = (input: string, language: CyrillicLanguage) => {
-    let preset: "ru" | "uk" | "mn";
-    if (language === "uk") {
-        preset = "uk";
-    } else if (language === "mn") {
-        preset = "mn";
-    } else {
-        preset = "ru";
-    }
-    const transliteration = cyrillicToTranslit({ preset }).transform(input);
+/**
+ * Adapted from cyrillic-to-translit-js
+ * Original author: Aleksandr Filatov
+ * License: MIT
+ * https://github.com/greybax/cyrillic-to-translit-js
+ */
 
-    return transliteration;
+export const romanizeCyrillic = (
+    input: string,
+    preset: CyrillicLanguage
+): string => {
+    if (!input) {
+        return "";
+    }
+
+    const { firstAssociations, nonFirstAssociations } =
+        getAssociationsForPreset(preset);
+
+    // We must normalize string for transform all unicode chars to uniform form
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+    const normalizedInput = input.normalize();
+
+    let newStr = "";
+    let isWordBoundary = false;
+    const vowels = "аеёиоуыэюяіїґәөүұhєі";
+
+    for (let i = 0; i < normalizedInput.length; i++) {
+        const isUpperCaseOrWhatever =
+            normalizedInput[i] === normalizedInput[i].toUpperCase();
+        let strLowerCase = normalizedInput[i].toLowerCase();
+
+        // Contextual character info for advanced rules
+        const prevChar = i > 0 ? normalizedInput[i - 1].toLowerCase() : "";
+        const nextChar =
+            i < normalizedInput.length - 1
+                ? normalizedInput[i + 1].toLowerCase()
+                : "";
+
+        if (strLowerCase === " ") {
+            newStr += " ";
+            isWordBoundary = true;
+            continue;
+        }
+
+        let newLetter;
+
+        // Handle contextual forms
+        if (preset === "kk" && strLowerCase === "ғ") {
+            // Example rule: ғ > ɣ between vowels, gh elsewhere
+            if (vowels.includes(prevChar) && vowels.includes(nextChar)) {
+                newLetter = "ɣ";
+            }
+        }
+
+        if (preset === "sr" && (strLowerCase === "ђ" || strLowerCase === "ћ")) {
+            // Example rule: use dj/tj before certain vowels or softeners?
+            const nextVowels = "еєиії";
+            if (nextVowels.includes(nextChar)) {
+                newLetter = strLowerCase === "ђ" ? "dj" : "tj";
+            }
+        }
+
+        if (
+            preset === "uk" &&
+            normalizedInput.slice(i - 1, i + 1).toLowerCase() === "зг"
+        ) {
+            // handle ukrainian special case зг > zgh
+            newLetter = "gh";
+        } else if (
+            typeof newLetter === "undefined" &&
+            (i === 0 || isWordBoundary)
+        ) {
+            newLetter = firstAssociations[strLowerCase];
+            isWordBoundary = false;
+        } else if (typeof newLetter === "undefined") {
+            newLetter = nonFirstAssociations[strLowerCase];
+        }
+
+        if ("undefined" === typeof newLetter) {
+            newStr += isUpperCaseOrWhatever
+                ? strLowerCase.toUpperCase()
+                : strLowerCase;
+        } else if (isUpperCaseOrWhatever) {
+            // handle multi-symbol letters
+            newLetter.length > 1
+                ? (newStr += newLetter[0].toUpperCase() + newLetter.slice(1))
+                : (newStr += newLetter.toUpperCase());
+        } else {
+            newStr += newLetter;
+        }
+    }
+
+    // Convert any remaining Cyrillic homoglyphs to their Latin counterparts.
+    const normalizedOutput = replaceCyrillicHomoglyphs(newStr);
+
+    return normalizedOutput;
+};
+
+type Associations = {
+    firstAssociations: Record<string, string>;
+    nonFirstAssociations: Record<string, string>;
+};
+
+const getAssociationsForPreset = (preset: CyrillicLanguage): Associations => {
+    /*
+    ASSOCIATIONS FOR INITIAL POSITION
+    */
+
+    // letters shared between languages
+    const firstLetters = {
+        а: "a",
+        б: "b",
+        в: "v",
+        д: "d",
+        з: "z",
+        й: "y",
+        к: "k",
+        л: "l",
+        м: "m",
+        н: "n",
+        о: "o",
+        п: "p",
+        р: "r",
+        с: "s",
+        т: "t",
+        у: "u",
+        ф: "f",
+        ь: "",
+    };
+
+    // digraphs appearing only in initial position
+    let initialDigraphs: Record<string, string> = {};
+
+    // language-specific mappings
+    switch (preset) {
+        case "ru":
+            Object.assign(firstLetters, {
+                г: "g",
+                и: "i",
+                ъ: "",
+                ы: "i",
+                э: "e",
+            });
+            initialDigraphs = { е: "ye" };
+            break;
+        case "uk":
+            Object.assign(firstLetters, {
+                г: "h",
+                ґ: "g",
+                е: "e",
+                и: "y",
+                і: "i",
+            });
+            initialDigraphs = { є: "ye", ї: "yi" };
+            break;
+        case "mn":
+            Object.assign(firstLetters, {
+                г: "g",
+                ө: "o",
+                ү: "u",
+                и: "i",
+                ы: "y",
+                э: "e",
+                ъ: "",
+            });
+            break;
+        case "be":
+            Object.assign(firstLetters, {
+                і: "i",
+                ў: "ŭ",
+                э: "e",
+                и: "i",
+            });
+            initialDigraphs = { е: "ye" };
+            break;
+        case "bg":
+            Object.assign(firstLetters, {
+                ъ: "a",
+                ь: "",
+                и: "i",
+            });
+            break;
+        case "ky":
+            Object.assign(firstLetters, {
+                ң: "ñ",
+                ө: "ö",
+                ү: "ü",
+                и: "i",
+                г: "g",
+                ы: "y",
+                э: "e",
+                ъ: "",
+            });
+            break;
+        case "kk":
+            Object.assign(firstLetters, {
+                ә: "ä",
+                ғ: "gh",
+                қ: "q",
+                ң: "ng",
+                ө: "ö",
+                ұ: "ū",
+                ү: "ü",
+                h: "h",
+                і: "i",
+                и: "i",
+            });
+            break;
+        case "mk":
+            Object.assign(firstLetters, {
+                ѓ: "gj",
+                ѕ: "dz",
+                ј: "j",
+                љ: "lj",
+                њ: "nj",
+                ќ: "ḱ",
+                и: "i",
+                г: "g",
+                ы: "y",
+                э: "e",
+                ъ: "",
+            });
+            initialDigraphs = { ѓ: "gj", ќ: "ḱ" };
+            break;
+        case "sr":
+            Object.assign(firstLetters, {
+                ђ: "đ",
+                ј: "j",
+                љ: "lj",
+                њ: "nj",
+                ћ: "ć",
+                џ: "dž",
+                и: "i",
+                г: "g",
+                ы: "y",
+                э: "e",
+                ъ: "",
+            });
+            initialDigraphs = { ђ: "đ", ћ: "ć", џ: "dž" };
+            break;
+        case "tg":
+            Object.assign(firstLetters, {
+                ъ: "ʾ",
+                ҳ: "h",
+                ӣ: "ī",
+                ҷ: "j",
+                ӯ: "ū",
+                и: "i",
+                г: "g",
+                ы: "y",
+                э: "e",
+            });
+            initialDigraphs = { ҷ: "j" };
+            break;
+        default:
+            break;
+    }
+
+    if (["uk", "be", "kk", "tg"].includes(preset)) {
+        Object.assign(firstLetters, {
+            "'": "",
+            "’": "",
+            ʼ: "",
+        });
+    }
+
+    // digraphs appearing in all positions
+    const regularDigraphs = {
+        ё: "yo",
+        ж: "zh",
+        х: "kh",
+        ц: "ts",
+        ч: "ch",
+        ш: "sh",
+        щ: "shch",
+        ю: "yu",
+        я: "ya",
+    };
+
+    const firstDigraphs = Object.assign({}, regularDigraphs, initialDigraphs);
+
+    const firstAssociations = Object.assign(firstLetters, firstDigraphs);
+
+    /*
+    ASSOCIATIONS FOR NON-INITIAL POSITION
+    */
+
+    const nonFirstLetters = Object.assign({}, firstLetters, { й: "i" });
+
+    // digraphs appearing only in non-initial positions
+    let nonInitialDigraphs: Record<string, string> = {};
+
+    // language-specific mappings
+    switch (preset) {
+        case "ru":
+            Object.assign(nonFirstLetters, { е: "e" });
+            break;
+        case "uk":
+            Object.assign(nonFirstLetters, { ї: "i" });
+            nonInitialDigraphs = {
+                є: "ie",
+                ю: "iu",
+                я: "ia",
+            };
+            break;
+        case "mn":
+            Object.assign(nonFirstLetters, { е: "e" });
+            break;
+        case "be":
+            nonInitialDigraphs = {
+                е: "ie",
+                ю: "iu",
+                я: "ia",
+            };
+            break;
+        case "mk":
+            nonInitialDigraphs = {
+                ѓ: "gj",
+                ќ: "ḱ",
+            };
+            break;
+        case "sr":
+            nonInitialDigraphs = {
+                ђ: "đ",
+                ћ: "ć",
+                џ: "dž",
+            };
+            break;
+        case "tg":
+            nonInitialDigraphs = {
+                ҷ: "j",
+            };
+            break;
+        default:
+            break;
+    }
+
+    const nonFirstDigraphs = Object.assign(regularDigraphs, nonInitialDigraphs);
+
+    const nonFirstAssociations = Object.assign(
+        nonFirstLetters,
+        nonFirstDigraphs
+    );
+
+    return { firstAssociations, nonFirstAssociations };
+};
+
+const replaceCyrillicHomoglyphs = (input: string): string => {
+    const homoglyphReplacements: Record<string, string> = {
+        е: "e",
+        а: "a",
+        о: "o",
+        р: "p",
+        с: "c",
+        у: "y",
+        х: "x",
+        к: "k",
+        м: "m",
+        т: "t",
+        в: "b",
+        н: "h",
+        ј: "j", // optional if you're transliterating this anyway
+    };
+
+    const output = input.replace(
+        /[еаорсухкмтвнј]/g,
+        (char) => homoglyphReplacements[char] || char
+    );
+
+    return output;
 };
