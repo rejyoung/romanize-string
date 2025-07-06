40f2a0307fd74132b225729f813535e392b22355
Ryan Young <youngre511@gmail.com>
2025-07-06 10:10:07 -0400

    fix: test failing due to incorrect indentation in test definition

--- test/korean-romanization/ko.test.ts
+++ test/korean-romanization/ko.test.ts
@@ -54,13 +54,13 @@ describe("romanizeHangul1", () => {
         expect(romanize("버")).toEqual("beo");
         expect(
             romanize(`훌쩍 커버렸어
-        함께한 기억처럼
-        널 보는 내 마음은
-        어느새 여름 지나 가을`)
+            함께한 기억처럼
+            널 보는 내 마음은
+            어느새 여름 지나 가을`)
         ).toEqual(`huljjeok keobeoryeosseo
-    hamkkehan gieokcheoreom
-    neol boneun nae maeumeun
-    eoneusae yeoreum jina gaeul`);
+            hamkkehan gieokcheoreom
+            neol boneun nae maeumeun
+            eoneusae yeoreum jina gaeul`);
     });
 });
 
