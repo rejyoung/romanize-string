e9a703d8035eda4577df0a812eba9a01dc363e63
Ryan Young <youngre511@gmail.com>
2025-08-14 19:01:27 -0400

    refactor: fix incorrect github actions and modify thai-romanizer to auto-download required binaries on install

--- packages/thai-romanizer/src/setup.ts
+++ packages/thai-romanizer/src/setup.ts
@@ -1,37 +1,43 @@
 import { registerPlugin } from "romanize-string/plugins";
 import { thaiRomanizer } from "./thai-romanizer.js";
+import { fileURLToPath } from "url";
 import os from "os";
 import path from "path";
-import { downloadBinaryIfNeeded } from "./download-binary-if-needed.js";
+import { access } from "fs/promises";
 
 export let binPath: string | undefined;
 
 export const setup = async () => {
-    const platform = os.platform();
-    const binaryName = getBinaryName(platform);
-    const binaryPath = path.join(__dirname, "bin", binaryName);
+    const binaryName = getBinaryName();
 
-    await downloadBinaryIfNeeded(binaryName, binaryPath);
+    const currentFilename = fileURLToPath(import.meta.url);
+    const dir = path.dirname(currentFilename);
+    const parentDir = path.dirname(dir);
 
-    binPath = binaryPath;
+    const binaryPath = path.join(parentDir, "bin", binaryName);
 
-    registerPlugin("th", thaiRomanizer);
+    try {
+        await access(binaryPath);
+
+        binPath = binaryPath;
+        registerPlugin("th", thaiRomanizer);
+    } catch (error) {
+        console.warn(
+            "Cannot register plugin. The thaiRomanizer binary is missing or was not successfully downloaded."
+        );
+    }
 };
 
-const getBinaryName = (platform: string): string => {
-    let name: string;
-    switch (platform) {
-        case "win32":
-            name = "thai-win.exe";
-            break;
-        case "darwin":
-            name = "thai-mac";
-            break;
-        case "linux":
-            name = "thai-linux";
-            break;
-        default:
-            throw new Error(`Unsupported platform: ${platform}`);
-    }
-    return name;
+const getBinaryName = (): string => {
+    const filenameMap = {
+        "darwin-arm64": "thai-mac-arm64",
+        "darwin-x64": "thai-mac-x64",
+        "linux-arm64": "thai-linux-arm64",
+        "linux-x64": "thai-linux-x64",
+        "win32-arm64": "thai-win-x64.exe",
+        "win32-x64": "thai-win-x64.exe",
+    };
+    const key = `${os.platform()}-${process.arch}`;
+
+    return filenameMap[key];
 };
